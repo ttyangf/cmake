0d8d36d0dc8f89116bf8b5a6f11bb367db2c23c0
Brad King <brad.king@kitware.com>
2015-11-30 12:50:25 -0500

    fixup! CPack/DragNDrop: Allow single license for multiple languages

--- Source/CPack/cmCPackDragNDropGenerator.h
+++ Source/CPack/cmCPackDragNDropGenerator.h
@@ -48,7 +48,7 @@ protected:
 
 private:
   std::string slaDirectory;
-  bool singleLicense = false;
+  bool singleLicense;
 
   void WriteLicense(cmGeneratedFileStream& outputStream, int licenseNumber,
     std::string licenseLanguage, std::string licenseFile = "");
